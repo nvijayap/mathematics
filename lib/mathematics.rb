6cbb223f53df92bd5a82b30ddbb5775ce80f2c85
Naga Vijayapuram <naga@MacBookPro-406C8F4B6B9A-2.local>
2013-10-01 11:04:47 -0700

    Refined support for tan

--- lib/mathematics.rb
+++ lib/mathematics.rb
@@ -138,18 +138,20 @@ module Mathematics
       degrees = (a[0] / Math::PI * 180) % 360 # think in degrees for ease
       h[degrees]
     when "tan" # rely on known knowledge first, before relying on Math module
-      h = {}
-      h["0.0"] = 0
-      h["45.0"] = 1
-      h["90.0"] = 1.0/0
-      h["135.0"] = -1
-      h["180.0"] = 0
-      h["225.0"] = 1
-      h["270.0"] = -1.0/0
-      h["315.0"] = -1
-      h.default = Math.tan(a[0])
+      h = Hash.new(Math.tan(a[0]))
+      h[0] = 0
+      h[45] = 1
+      h[90] = 1.0/0
+      h[135] = -1
+      h[180] = 0
+      h[225] = 1
+      h[270] = -1.0/0
+      h[315] = -1
       degrees = (a[0] / Math::PI * 180) % 360 # think in degrees for ease
-      degrees = ('%.1f' % degrees).to_s
+      # to deal with 45, 135, 225, 315 ...
+      quantum = 1.0e-12
+      degrees = degrees.ceil if degrees.ceil - degrees < quantum
+      degrees = degrees.floor if degrees - degrees.floor < quantum
       h[degrees]
     end
   end
